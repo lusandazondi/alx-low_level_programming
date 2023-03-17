c9337ed638a9a0cc7c410b0b109efcd7596679e2
lusandazondi <lusanda.zondi@yahoo.com>
2023-03-17 14:47:57 -0700

    numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,16 +3,17 @@
 /**
  * main - Prints all single digit numbers of base 10 starting from 0
  *
- * Return: Always 0 (success)
+ * Return: 0
  */
 int main(void)
 {
-int n = 48;
-while (n <= 57)
-{
-	putchar(n);
-	n += 1;
-}
-printf("\n");
-return (0);
+	int n = 48;
+
+	while (n <= 57)
+	{
+		putchar(n);
+		n += 1;
+	}
+	printf("\n");
+	return (0);
 }
