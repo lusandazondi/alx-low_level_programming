51f6089712b2e31833555979cea0c9f690b59332
lusandazondi <lusanda.zondi@yahoo.com>
2023-03-19 05:01:01 -0700

    numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,13 +7,11 @@
  */
 int main(void)
 {
-	int n = 48;
+	int num;
 
-	while (n <= 57)
-	{
-		putchar(n);
-		n += 1;
-	}
-	printf("\n");
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
+	putchar('\n');
 	return (0);
 }
